12527ced6073c8ca6e153cf00c3e89a576f9ad26
kolonel <carlfakhry@gmail.com>
2018-02-12 15:56:04 -0500

    Updated documentation

--- R/ProcessData.R
+++ R/ProcessData.R
@@ -15,12 +15,12 @@ getGeneVals <- function(trguids, gene_expression_data){
 
 #' This function runs a causal relation engine by computing the Quaternary Dot
 #' Product Scoring Statistic, Ternary Dot Product Scoring Statistic or the Enrichment test over the Homo
-#' Sapien STRINGdb causal network (version 10 provided under the creative commons license: 
+#' Sapien STRINGdb causal network (version 10 provided under the Creative Commons license: 
 #' https://creativecommons.org/licenses/by/3.0/).
 #' 
 #' @description This function runs a causal relation engine by computing the Quaternary Dot
 #'              Product Scoring Statistic, Ternary Dot Product Scoring Statistic or the Enrichment test over the Homo
-#'              Sapien STRINGdb causal network (version 10 provided under the creative commons license: 
+#'              Sapien STRINGdb causal network (version 10 provided under the Creative Commons license: 
 #'              https://creativecommons.org/licenses/by/3.0/).
 #' 
 #' @usage RunCRE_HSAStringDB(gene_expression_data, method = "Quaternary", 
